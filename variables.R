72002fe6a02833fdc8b794b08812c8b5dcc87615
dnbronner <dbronner@umich.edu>
2016-01-05 11:38:57 -0500

    Added comment pertaining to git add, commit, and tracking changes

--- variables.R
+++ variables.R
@@ -6,5 +6,8 @@ x+y+z     *if you can change x or y to a character or logical
 it won't work     
 rm()      #removes variables
 cat filename  this is similar to the less command
-
+*Every time you make a change, you must do the git add and git 
+commit commands!*
+This is the Bash way of tacking changes - something I do in 
+Word
 mass
