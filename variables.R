c0165df643a56069bb4633d3bdc6dd3e0f97d096
dnbronner <dbronner@umich.edu>
2016-01-05 11:33:27 -0500

    Added comment on line x+y+z and added cat command

--- variables.R
+++ variables.R
@@ -2,7 +2,9 @@
 x = 1     #this doesn't execute
 y = x*2   #this is dependent upon x
 z = y^2   #this is dependent upon x and y
-x+y+z     
+x+y+z     *if you can change x or y to a character or logical 
+it won't work     
 rm()      #removes variables
+cat filename  this is similar to the less command
 
 mass
