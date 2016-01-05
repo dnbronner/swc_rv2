69ab95bad9e49f83b99a6181db17f0689d9ede3a
dnbronner <dbronner@umich.edu>
2016-01-05 11:31:18 -0500

    Added commet on +y and +z line

--- variables.R
+++ variables.R
@@ -1,8 +1,8 @@
 #This is the document where I learned introductory R
 x = 1     #this doesn't execute
-y = x*2
-z = y^2
-x+y+z
+y = x*2   #this is dependent upon x
+z = y^2   #this is dependent upon x and y
+x+y+z     
 rm()      #removes variables
 
 mass
